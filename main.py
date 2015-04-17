184c33d7528e61010116599f1ca3fbb68f1dc4a7
User Khrogos <khrogos@nova.khrogosphe.re>
2015-04-17 22:13:12 +0200

    add tkinter template

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3.4
+#coding: utf-8
+
+import tkinter as tk
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        # <create the rest of your GUI here>
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
